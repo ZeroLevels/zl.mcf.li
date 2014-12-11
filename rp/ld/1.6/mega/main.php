b56e8eb5b551cbe8e9bc1ccd5411f9beb9a87ce1
Zero <ZeroLevels@yahoo.com>
2014-12-11 04:40:01 -0500

    Updated download link for 1.6.

--- rp/ld/1.6/mega/main.php
+++ rp/ld/1.6/mega/main.php
@@ -1,3 +1,3 @@
 <?php header('Location:'.
-'https://mega.co.nz/#!gwMBXTZC!UedBQRgRYyHOvsGOA7PSfgUradIzsdyo3yQftW-Bqhc'
+'https://mega.co.nz/#!58M1wAhD!dV6ac5hM7uLdWqLdmkJJpmuw7Te6m-_XZwtOIB0EZr0'
 ) ;
